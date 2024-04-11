fc18d8c64a3421f683d81e91ae765284fb94f72e
Uttam <kk82171@gmail.com>
2024-04-11 21:12:34 +0530

    updated on screen keyboard

--- script.js
+++ script.js
@@ -3,57 +3,77 @@ const numpad = document.querySelector("#numpad");
 const buttons = numpad.querySelectorAll(":scope > button");
 const reset = numpad.querySelector(":scope > button:nth-child(17)");
 const result = numpad.querySelector(":scope > button:nth-child(18)");
+const del = numpad.querySelector(":scope > button:nth-child(4)");
 const keys_to_prevent_at_beginning = ["+", "/", "*", ".", "-"];
 
-document.addEventListener("keyup", e => {
-  if (input_value.value == "")
-    return (input_value.value = e.target.value.replace(/[^0-9\-]/g, ""));
+function sanitizeExpression(expression) {
+  return expression.replace(/x/g, "*");
+}
 
-  return (input_value.value = e.target.value.replace(/[^0-9+\-*/]/g, ""));
-});
+const calc = () => {
+  input_value.value = sanitizeExpression(input_value.value);
+  try {
+    input_value.value = eval(input_value.value);
+    if (!isFinite(input_value.value)) {
+      alert("Invalid expression");
+      input_value.value = "";
+    }
+  } catch (error) {}
+};
 
-document.addEventListener("keydown", e => {
-  input_value.focus();
-  // const keys_to_prevent = ["+", "/", "*", "."];
-  // if (keys_to_prevent.includes(e.key) && input_value.value == "") {
-  //   e.preventDefault();
-  // }
-  // if (
-  //   input_value.value == "" && !(e.key < 48 || e.key > 57 || e.key < 96 || e.key > 105)
-  // ) {
-  //   e.preventDefault();
-  // }
-  // if (
-  //   ["109", "106", "110", "111", "107", "189", "190", "191", ""].indexOf(
-  //     e.key
-  //   ) !== -1
-  // )
-  //   return;
-  // if (!(e.key < 48 || e.key > 57) && !(e.key < 96 || e.key > 105)) {
-  //   e.preventDefault();
-  // }
-  // if (e.key != Number || !e.key.includes(keys_to_prevent)) e.preventDefault();
-
+const append = operator => {
   if (
-    [46, 8, 9, 27, 13].indexOf(e.key) !== -1 ||
-    // Allow: Ctrl+A
-    (e.key === 65 && e.ctrlKey === true) ||
-    // Allow: home, end, left, right
-    (e.key >= 35 && e.key <= 39)
-  ) {
-    return;
-  }
-  // Ensure that it is a number and stop the keypress
-  // if (!(e.key < 48 || e.key > 57) || !(e.key < 96 || e.key > 105)) {
-  //   e.preventDefault();
-  // }
-});
+    input_value.value.slice(-1) !== "-" &&
+    input_value.value.slice(-1) !== "+" &&
+    input_value.value.slice(-1) !== "/" &&
+    input_value.value.slice(-1) !== "x" &&
+    input_value.value.slice(-1) !== "." &&
+    input_value.value !== ""
+  )
+    input_value.value += operator;
+};
+
+// document.addEventListener("keyup", e => {
+//   if (e.key == "-" && input_value.value == "") {
+//     console.log("working");
+//     return (input_value.value = e.target.value.replace(/[^0-9\-]/g, ""));
+//   }
+
+//   return (input_value.value = e.target.value.replace(/[^0-9+\-*./]/g, ""));
+// });
+
+// document.addEventListener("keydown", e => {
+//   input_value.focus();
+//   console.log(e);
+//   if (e.key == "Enter") calc();
+
+//   if (
+//     [46, 8, 9, 27, 13].indexOf(e.key) !== -1 ||
+//     (e.key === 65 && e.ctrlKey === true) ||
+//     (e.key >= 48 && e.key <= 57)
+//   ) {
+//     return;
+//   } else {
+//     e.preventDefault();
+//     return false;
+//   }
+// });
 
 buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    if (button.textContent != "DEL" && button.textContent != "=")
+  button.addEventListener("click", e => {
+    if (input_value.value == "" && e.target.textContent == "-")
+      input_value.value = "-";
+
+    if (
+      e.target.textContent !== "DEL" &&
+      e.target.textContent != "=" &&
+      e.target.textContent != "-" &&
+      e.target.textContent != "+" &&
+      e.target.textContent != "/" &&
+      e.target.textContent != "." &&
+      e.target.textContent != "x"
+    )
       input_value.value += button.textContent;
-    input_value.focus();
   });
 });
 
@@ -62,16 +82,8 @@ reset.addEventListener("click", () => {
   input_value.blur();
 });
 
-result.addEventListener("click", () => {
-  if (/(--)|(\+\+)|(\*\*)|(\/\/)|([\-+*/]\s*[+\-*/])/.test(input_value.value)) {
-    alert("Invalid expression");
-    return;
-  }
-  try {
-    input_value.value = eval(input_value.value);
-    if (!isFinite(input_value.value)) {
-      alert("Invalid expression");
-      input_value.value = "";
-    }
-  } catch (error) {}
+result.addEventListener("click", calc);
+
+del.addEventListener("click", () => {
+  input_value.value = input_value.value.slice(0, -1);
 });
